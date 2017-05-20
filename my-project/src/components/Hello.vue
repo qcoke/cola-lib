9e0517e061e4b2853f49ca25b318726e8c3bab16
可乐加糖 <black.angel.liu@gmail.com>
2017-05-20 10:25:23 +0800

    Blog Vue 练习结束。

---
+++ my-project/src/components/Hello.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <span>{{count}}</span>
+    <button v-on:click="inc">+</button>
+  </div>
+</template>
+<script>
+export default {
+   data () {
+      return {count: 0}
+    },
+    methods: {
+      inc () {this.count++}
+    }
+}
+</script>
+<style scoped>
+  span{color:red}
+</style>
