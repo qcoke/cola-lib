0c556943816e378f9887ef3566b7a36e067f4da4
刘灿 <black.angel.liu@gmail.com>
2019-01-04 10:10:15 +0800

    使用 webpack 练习到了加载图片和文字库。

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,8 +1,18 @@
 import _ from 'lodash';
+import './style.css';
+import logo from './webpack-es6.png';
 
-function component(){
+function component() {
   var element = document.createElement('div');
-  element.innerHTML = _.join(['Hello','webpack'], ' ');
+  // 将图像添加到我们现有的DIV
+  var logoImg = new Image();
+
+  element.innerHTML = _.join(["hello", "webpack"], " ");
+  element.classList.add('hello');
+
+  logoImg.src = logo;
+  element.appendChild(logoImg);
   return element;
 }
+
 document.body.appendChild(component());
